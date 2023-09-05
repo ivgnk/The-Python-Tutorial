23049d3d40ec920bbc851393a335115f50b9a501
Ivan Genik <IVGenik@rambler.ru>
2023-09-05 15:38:16 +0500

    Add files via upload

--- 10_Brief Tour of the Standard Library.py
+++ 10_Brief Tour of the Standard Library.py
@@ -5,6 +5,8 @@ import os
 import shutil
 import sys
 import numpy as np
+from timeit import Timer
+import doctest
 
 def with_os_module_func():
     function_name = inspect.currentframe().f_code.co_name
@@ -33,14 +35,34 @@ def wildcard_func():
     print(f'{function_name=} \n')
     print('Files in directory = ',glob.glob('*.py'))
 
+
 def insys_func():
     function_name = inspect.currentframe().f_code.co_name
     print(f'{function_name=} \n')
-    print(sys.stderr.write('Warning, log file not found,\nstarting a new one\n'))
+    # print(sys.stderr.write('Warning, log file not found,\nstarting a new one\n'))
+
+    print(sys.argv)
+
+def performance_measurement():
+    # In contrast to timeit’s fine level of granularity, the profile and pstats modules
+    # provide tools for identifying time critical sections in larger blocks of code.
+    time1 = Timer('t=a; a=b; b=t', 'a=1; b=2').timeit()
+    print(f' {time1 = } ')
+    time2 = Timer('a,b = b,a', 'a=1; b=2').timeit()
+    print(f' {time2 = } ')
+
+## def quality_control
+## def average(values):
+##     """Computes the arithmetic mean of a list of numbers.
+##     >>> print(average([20, 30, 70]))
+##     40.0
+##     """
+##     return sum(values) / len(values)
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # with_os_module_func()
     # wildcard_func()
     # insys_func()
-    print(math.ulp(0.1))
+    # performance_measurement()
+   ## doctest.testmod()
