44e62d574528a813e34f6dda350b7a06b58863ee
Ivan Genik <IVGenik@rambler.ru>
2023-09-05 14:26:39 +0500

    Add files via upload

--- 15_Floating Point Arithmetic.py
+++ 15_Floating Point Arithmetic.py
@@ -19,7 +19,7 @@ def as_integer_ratio():
     print( x == dat )
     print( x ==  round(x_i[0]/x_i[1],n ))
 
-def summa():
+def summa_func():
     function_name = inspect.currentframe().f_code.co_name
     print(f'{function_name=}')
     llst = [0.1] * 10
@@ -27,7 +27,22 @@ def summa():
     print('Традиционная сумма = ',sum(llst) == 1.0)        #  False
     print('Сумма из математики = ',math.fsum(llst) == 1.0) #    True
 
+def divmod_func():
+    # Return the tuple (x//y, x%y). Invariant: div*y + mod == x.
+    function_name = inspect.currentframe().f_code.co_name
+    print(f'{function_name=}')
+    data = 2 ** 56
+    print(f'{data=}')
+    ydiv = 10
+    q, r = divmod(data, ydiv)
+    print(f'{q=}')
+    print(f'{r=}')
+    the_restore_ = q*ydiv + r
+    print(f'{the_restore_=}')
+    print(the_restore_ == data)
+
 if __name__ == "__main__":
     # round_data()
     # as_integer_ratio()
-    summa()
+    # summa_func()
+    divmod_func()
